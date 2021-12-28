c6df493820920d20b3b88aa7f0887c50f09307e4
Your Name <ubaydullayev@samtuit.uz>
2021-12-29 00:10:49 +0500

    12-misol

--- logic12.py
+++ logic12.py
@@ -1,4 +1,7 @@
 def main(a):
+    num1 = a % 10
+    num2 = a // 10
+    b = num1 == num2
     """
     Given a two-digit integer a,  check the following statement "All digits of the number are the same".
     Args:
@@ -6,4 +9,4 @@ def main(a):
     Returns:
         True if all digits of a are the same, False otherwise
     """
-    return
+    return b
