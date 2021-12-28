d65d75986ad34ab712e0457e63f7857029c8a00f
Your Name <ubaydullayev@samtuit.uz>
2021-12-29 00:35:12 +0500

    17-misol

--- logic17.py
+++ logic17.py
@@ -1,4 +1,19 @@
 def main(a):
+    num1 = a % 10
+    a //= 10
+    
+    num2 = a % 10
+    a //= 10
+    
+    num3 = a % 10
+    a //= 10
+    
+    num4 = a % 10
+    a //= 10
+    
+    num5 = a % 10
+    
+    result = num5 <= num4 and num4 <= num3 and num3 <= num2 and num2 <= num1
     """
     Given a five-digit integer a,  check the following statement "All digits of the number are in ascending order".
     Args:
@@ -6,4 +21,4 @@ def main(a):
     Returns:
         True if all digits of a are in ascending order, False otherwise
     """
-    return
+    return result
