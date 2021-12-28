73ea14ef34e10c1bd33ef59886dfd171e59464f6
Your Name <ubaydullayev@samtuit.uz>
2021-12-29 00:16:52 +0500

    14-misol

--- logic14.py
+++ logic14.py
@@ -1,4 +1,7 @@
 def main(a):
+    num1 = a % 10
+    num2 = a // 10
+    result = (num1 + num2) % 2 != 0
     """
     Given a two-digit integer a,  check the following statement "All digits sum is odd".
     Args:
@@ -6,4 +9,4 @@ def main(a):
     Returns:
         True if all digits sum is odd, False otherwise
     """
-    return
+    return result
