0cd89a3731818f031cd7720d7013544034055316
Your Name <ubaydullayev@samtuit.uz>
2021-12-29 00:14:31 +0500

    13-misol

--- logic13.py
+++ logic13.py
@@ -1,4 +1,8 @@
 def main(a):
+    num1 = a % 10
+    num2 = a // 10
+    
+    result = (num1 + num2) % 2 == 0
     """
     Given a two-digit integer a,  check the following statement "All digits sum is even".
     Args:
@@ -6,4 +10,4 @@ def main(a):
     Returns:
         True if all digits sum is even, False otherwise
     """
-    return
+    return result
