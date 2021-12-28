e0636bc76255b202674594d2d4d2dae0cbdea046
Your Name <ubaydullayev@samtuit.uz>
2021-12-28 23:43:41 +0500

    6-misol

--- logic06.py
+++ logic06.py
@@ -1,4 +1,5 @@
 def main(a,b):
+    c = a >= 0 or b >= 0
     """
     Given two integers a, b,  check the following statement "At least one of the numbers 'a' and 'b' is positive".
     Args:
@@ -7,4 +8,4 @@ def main(a,b):
     Returns:
         True if at least one of the numbers 'a' and 'b' is positive, False otherwise
     """
-    return
+    return c
