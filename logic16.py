c40334464a0a563c92b752fc31d469fbb9e4f1d3
Your Name <ubaydullayev@samtuit.uz>
2021-12-29 00:27:49 +0500

    16-misol

--- logic16.py
+++ logic16.py
@@ -1,4 +1,22 @@
 def main(a):
+    num1 = pow(0, a)
+    a //= 10
+    
+    num2 = pow(0, a)
+    a //= 10
+    
+    num3 = pow(0, a)
+    a //= 10
+    
+    num4 = pow(0, a)
+    a //= 10
+    
+    num5 = pow(0, a)
+    a //= 10
+    
+    num6 = pow(0, a)
+    
+    result = num1 == 0 and num2 == 0 and num3 == 0 and num4 == 0 and num5 == 0 and num6 != 0
     """
     Given integer a,  check the following statement "The integer is a five-digit number".
     Args:
@@ -6,4 +24,4 @@ def main(a):
     Returns:
         True if a is five-digit number, False otherwise
     """
-    return
+    return result
