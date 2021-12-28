5c8c4e5707a34ca15e1bdccbfbfd773572460190
Your Name <ubaydullayev@samtuit.uz>
2021-12-28 23:34:25 +0500

    2-misol

--- logic02.py
+++ logic02.py
@@ -1,5 +1,5 @@
 def main(a,b):
-    b = a >= 0 and b >= 0
+    c = a >= 0 and b >= 0
     """
     Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is positive".
     Args:
@@ -8,4 +8,4 @@ def main(a,b):
     Returns:
         True if each of the numbers 'a' and 'a' is positive, False otherwise
     """
-    return b
+    return c
